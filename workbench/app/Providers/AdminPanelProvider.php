e6f4de58f519108a6e1f0089eedf6b793e91f8ba
ankitcodes4u <ankitcodes4u@gmail.com>
2025-06-28 06:03:12 +0545

    fix: reverting change in AdminPanelProvider inside workbench

--- workbench/app/Providers/AdminPanelProvider.php
+++ workbench/app/Providers/AdminPanelProvider.php
@@ -1,79 +1,59 @@
 <?php
 
-namespace Eclipse\Frontend\Providers;
+namespace Workbench\App\Providers;
 
-use Eclipse\Common\Providers\GlobalSearchProvider;
-use Eclipse\Core\Services\Registry;
-use Eclipse\Frontend\Filament\Pages;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
-use Filament\Support\Enums\Platform;
 use Filament\Support\Facades\FilamentView;
-use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use pxlrbt\FilamentEnvironmentIndicator\EnvironmentIndicatorPlugin;
 
-class FrontendPanelProvider extends PanelProvider
+class AdminPanelProvider extends PanelProvider
 {
-    private const PANEL_ID = 'frontend';
-
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->id(self::PANEL_ID)
-            ->path('')
-            ->colors([
-                'primary' => Color::Cyan,
-                'gray' => Color::Slate,
-            ])
-            ->authGuard(self::PANEL_ID)
-            ->topNavigation()
-            ->brandName(fn () => Registry::getSite()->name)
-            ->discoverResources(in: app_path('Filament/Frontend/Resources'), for: 'App\\Filament\\Frontend\\Resources')
-            ->discoverPages(in: app_path('Filament/Frontend/Pages'), for: 'App\\Filament\\Frontend\\Pages')
-            ->pages([
-                Pages\Home::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Frontend/Widgets'), for: 'App\\Filament\\Frontend\\Widgets')
-            ->widgets([])
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
                 StartSession::class,
+                AuthenticateSession::class,
                 ShareErrorsFromSession::class,
                 VerifyCsrfToken::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            ->globalSearch(GlobalSearchProvider::class)
-            ->globalSearchKeyBindings(['ctrl+k', 'command+k'])
-            ->globalSearchFieldSuffix(fn (): ?string => match (Platform::detect()) {
-                Platform::Windows, Platform::Linux => 'CTRL+K',
-                Platform::Mac => '⌘K',
-                default => null,
-            })
-            ->plugins([
-                EnvironmentIndicatorPlugin::make(),
+            ->authMiddleware([
+                Authenticate::class,
             ])
-            ->renderHook(
-                PanelsRenderHook::TOPBAR_END,
-                fn () => view('frontend-panel::filament.components.theme-switcher')
-            );
+            ->plugins([
+                FilamentShieldPlugin::make(),
+            ])
+            ->pages([
+                Dashboard::class,
+            ]);
     }
 
     public function register(): void
     {
         parent::register();
+
         FilamentView::registerRenderHook('panels::body.end', fn (): string => Blade::render("@vite('resources/js/app.js')"));
     }
 }
